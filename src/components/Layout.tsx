adb1fa84c7c561ec503d1ca1ff5be7caa44a6683
Лера Бильщук <leratheone@gmail.com>
2023-02-23 15:41:45 +0300

    add router

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <header className="header">
+        <ul className="header__menu">
+          <li>
+            <NavLink className="header__menu-link" to="/">О приложении</NavLink>
+          </li>
+          <li>
+            <NavLink className="header__menu-link" to="/quotes">Котировки</NavLink>
+          </li>
+        </ul>
+      </header>
+
+      <main className="main">
+        <Outlet />
+      </main>
+
+      <footer>2023</footer>
+    </>
+  );
+};
+
+export { Layout };
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return <>This page doesn't exist. Go <Link to="/">home</Link></>;
+};
+
+export { NotFound };
